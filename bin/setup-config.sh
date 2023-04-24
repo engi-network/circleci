b45d25481cb7a29a12f84c637cf7a3d8e64c1801
Mark Holcomb <markholcomb@engi.network>
2023-04-24 11:35:58 -0700

    Testing

--- bin/setup-config.sh
+++ bin/setup-config.sh
@@ -7,16 +7,6 @@ which yq || snap install yq
 # Setup config parameters
 declare -A PARAMS
 
-if [ -e config/parameters.json ]; then
-  read build_args < <(
-    jq -r 'to_entries[]|[.key,.value]|join(" ")' config/parameters.json |
-    while read k v; do
-      echo "--build-arg ${k^^}=$v"
-    done | xargs
-  )
-  jq '.build_args = "'"$build_args"'"' config/parameters.json
-fi
-
 branches=($(git for-each-ref --points-at HEAD --format '%(refname:lstrip=2)' refs/heads))
 tags=($(git for-each-ref --points-at HEAD --format '%(refname:lstrip=2)' refs/tags))
 
@@ -28,19 +18,40 @@ PARAMS[project]="$CIRCLE_PROJECT_REPONAME"
 
 if [[ "$CIRCLE_TAG" =~ ^v[0-9] ]]; then
   PARAMS[release]=true
-  PARAMS[environment]=production
-  PARAMS[primary_branch]=""
+  if [[ "$CIRCLE_PROJECT_REPONAME" == "blockchain" ]]; then
+    PARAMS[environment]=mainnet
+  else
+    PARAMS[environment]=production
+  fi
+  PARAMS[primary_branch]=main
 else
   PARAMS[release]=false
-  PARAMS[environment]=staging
-  PARAMS[primary_branch]=master
+  if [[ "$CIRCLE_PROJECT_REPONAME" == "blockchain" ]]; then
+    PARAMS[environment]=testnet
+  else
+    PARAMS[environment]=staging
+  fi
+  PARAMS[primary_branch]=main
 fi
+
+if [ -e config/parameters.json ]; then
+  read build_args < <(
+    jq -r 'to_entries[]|[.key,.value]|join(" ")' config/parameters.json |
+    while read k v; do
+      echo "--build-arg ${k^^}=$v"
+    done | xargs
+  )
+  jq '.build_args = "'"$build_args"'"' config/parameters.json
+else
+  echo '{}'
+fi |
 
 eval env -i $(
   for k in ${!PARAMS[@]}; do
     echo "$k=\${PARAMS[$k]}"
   done
-) jq 'env' \<<<'{}' |
+) jq 'env' |
+
 sed -r 's/"(false|true)"(,)?$/\1\2/' > .circleci/parameters.json
 
 yq ea '. as $item ireduce({}; . * $item )' .circleci/circleci/common/{headers,executors,parameters}.yml .circleci/circleci/common/{commands,jobs}/*.yml .circleci/circleci/${CIRCLE_PROJECT_REPONAME}.yml > .circleci/project.yml
