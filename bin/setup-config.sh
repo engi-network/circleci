4371ec6c9c08ba31ab7d7a921eaf629e0407ac1a
Mark Holcomb <markholcomb@engi.network>
2023-04-24 11:25:31 -0700

    Testing

--- bin/setup-config.sh
+++ bin/setup-config.sh
@@ -4,6 +4,9 @@ set -x
 
 which yq || snap install yq
 
+# Setup config parameters
+declare -A PARAMS
+
 if [ -e config/parameters.json ]; then
   read build_args < <(
     jq -r 'to_entries[]|[.key,.value]|join(" ")' config/parameters.json |
@@ -11,11 +14,33 @@ if [ -e config/parameters.json ]; then
       echo "--build-arg ${k^^}=$v"
     done | xargs
   )
-
   jq '.build_args = "'"$build_args"'"' config/parameters.json
+fi
+
+branches=($(git for-each-ref --points-at HEAD --format '%(refname:lstrip=2)' refs/heads))
+tags=($(git for-each-ref --points-at HEAD --format '%(refname:lstrip=2)' refs/tags))
+
+echo "[branches are: ${branches[@]}]"
+echo "[tags are: ${tags[@]}]"
+
+PARAMS[build_args]="$build_args"
+PARAMS[project]="$CIRCLE_PROJECT_REPONAME"
+
+if [[ "$CIRCLE_TAG" =~ ^v[0-9] ]]; then
+  PARAMS[release]=true
+  PARAMS[environment]=production
+  PARAMS[primary_branch]=""
 else
-  echo '{}'
-fi |
-jq '.project = "'$CIRCLE_PROJECT_REPONAME'"' > .circleci/parameters.json
+  PARAMS[release]=false
+  PARAMS[environment]=staging
+  PARAMS[primary_branch]=master
+fi
+
+eval env -i $(
+  for k in ${!PARAMS[@]}; do
+    echo "$k=\${PARAMS[$k]}"
+  done
+) jq 'env' \<<<'{}' |
+sed -r 's/"(false|true)"(,)?$/\1\2/' > .circleci/parameters.json
 
 yq ea '. as $item ireduce({}; . * $item )' .circleci/circleci/common/{headers,executors,parameters}.yml .circleci/circleci/common/{commands,jobs}/*.yml .circleci/circleci/${CIRCLE_PROJECT_REPONAME}.yml > .circleci/project.yml
